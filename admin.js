e3724e96f09b8f790f9fc1db41006f77b8905be5
Umedul <129896711+Umedul@users.noreply.github.com>
2025-05-01 06:27:11 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -17,8 +17,8 @@ function login() {
 // Function to fetch teams from the backend
 function fetchTeams() {
     fetch('https://spl-auction-backend.onrender.com/api/teams')  // Updated URL
-        .then(response => response.json())
-        .then(teams = {
+        .then(response = response.json())
+        .then(teams => {
             renderTeams(teams);
         })
         .catch(error => console.error('Error fetching teams:', error));
